99f23599e685a96caf149dcec23e76df48f7ef86
alex <alexstepanyak@gmail.com>
2025-01-21 12:09:38 +0200

    back: fastify-cors reinstalled, cors configured. front: create simple page for communication with AI

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import chatRoutes from "./routes/ChatRoutes";
 import {AppConfig} from "./configs/AppConfig";
 import {validateEnv} from "./helpers/EnvCheck";
 import rootRoutes from "./routes/RootRoutes";
+import fastifyCors from "@fastify/cors";
 
 dotenv.config();
 export const server = Fastify();
@@ -11,6 +12,13 @@ export const server = Fastify();
 // creating global dependencies
 const appConfig = new AppConfig(process.env.GEMINI_API_KEY, process.env.PORT);
 server.decorate("appConfig", appConfig);
+
+// configuring cors
+server.register(fastifyCors, {
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+});
 
 // registration of new API routes
 server.register(chatRoutes);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [myMessage, setMyMessage] = useState("");
+    const [aiMessage, setAIMessage] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    async function sendMessage() {
+        const resp = await fetch("http://localhost:5001/chat", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                message: myMessage
+            })
+        });
+        const data = await resp.json();
+        setAIMessage(data.response);
+        console.log(data);
+    }
+
+    return (
+        <>
+            <h1>Depression AI Chat</h1>
+            <div>
+                <p>
+                    Message your: {myMessage}
+                </p>
+                <input onChange={(e) => setMyMessage(e.target.value)} value={myMessage} />
+                <p>
+                    Message AI: {aiMessage}
+                </p>
+            </div>
+            <div className="card">
+                <button onClick={sendMessage}>
+                    Send Message
+                </button>
+            </div>
+        </>
+    )
 }
 
 export default App
